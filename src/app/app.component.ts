79b355bd8cc389666e4dd9804579ac7b1a54ad15
davitGarsia <davitgarsiashviligmail.com>
2022-12-01 16:56:13 +0400

    finish app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,15 +9,37 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class AppComponent implements OnInit {
   title = 'Ng-Forms';
   cvForm!: FormGroup;
+  //mailValidator = '^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$';
 
   ngOnInit() {
     this.cvForm = new FormGroup({
-      firstName: new FormControl(null, Validators.required),
+      firstName: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(2),
+      ]),
       lastName: new FormControl(null, Validators.required),
-      email: new FormControl(null, [Validators.required, Validators.email]),
-      phone: new FormControl(null, Validators.required),
-      ghLink: new FormControl(null, Validators.required),
-      linkedIn: new FormControl(null, Validators.required),
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$'),
+      ]),
+      phone: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(9),
+        Validators.maxLength(12),
+        Validators.pattern('[0-9]+'),
+      ]),
+      ghLink: new FormControl(null, [
+        Validators.required,
+        Validators.pattern(
+          '^(http(s?)://)?(www.)?github.([a-z])+/([A-Za-z0-9]{1,})+/?$'
+        ),
+      ]),
+      linkedIn: new FormControl(null, [
+        Validators.required,
+        Validators.pattern(
+          '^(http(s?)://)?(www.)?linkedin.([a-z])+/([A-Za-z0-9]{1,})+/?$'
+        ),
+      ]),
       workPlace: new FormControl(null, Validators.required),
       workStart: new FormControl(null, Validators.required),
       workEnd: new FormControl(null, Validators.required),
@@ -30,4 +52,6 @@ export class AppComponent implements OnInit {
   onSubmit() {
     console.log(this.cvForm);
   }
+
+  //validateEmail(control: FormControl): { [s: string]: boolean } {}
 }
